f907c754cd4c68e61da9b3bb91e19ad8f3fcb665
dingzhengjie <53457219+dingzhengjie@users.noreply.github.com>
2026-05-09 17:54:07 +0800

    更新 HomeActivity.java

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/HomeActivity.java
@@ -122,7 +122,7 @@ public class HomeActivity extends BaseActivity implements CustomTitleView.Listen
                 android.content.Intent intent = new android.content.Intent(this, LiveActivity.class);
                 startActivity(intent);
                 // 这里不写 finish()，返回时就能回到首页
-            }, 3000);
+            }, 6000);
         }
     }
     @Override
