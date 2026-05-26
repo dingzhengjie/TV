f4f38575b9596738a89e17d423d171d8c93666f4
dingzhengjie <53457219+dingzhengjie@users.noreply.github.com>
2026-05-26 14:42:03 +0800

    Revert accidental replacement of LiveActivity.java and restore original content from previous commit

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/LiveActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/LiveActivity.java
@@ -1,1 +1,88 @@
-[REPLACED_FILE_CONTENT]
+package com.fongmi.android.tv.ui.activity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.leanback.widget.OnChildViewHolderSelectedListener;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.media3.common.C;
+import androidx.media3.common.MediaMetadata;
+import androidx.media3.common.Player;
+import androidx.media3.common.PlaybackException;
+import androidx.media3.common.VideoSize;
+import androidx.media3.ui.PlayerView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewbinding.ViewBinding;
+
+import com.bumptech.glide.request.transition.Transition;
+import com.fongmi.android.tv.App;
+import com.fongmi.android.tv.Constant;
+import com.fongmi.android.tv.R;
+import com.fongmi.android.tv.Setting;
+import com.fongmi.android.tv.api.config.LiveConfig;
+import com.fongmi.android.tv.bean.Channel;
+import com.fongmi.android.tv.bean.Config;
+import com.fongmi.android.tv.bean.Epg;
+import com.fongmi.android.tv.bean.EpgData;
+import com.fongmi.android.tv.bean.Group;
+import com.fongmi.android.tv.bean.Keep;
+import com.fongmi.android.tv.bean.Live;
+import com.fongmi.android.tv.bean.Result;
+import com.fongmi.android.tv.bean.Track;
+import com.fongmi.android.tv.databinding.ActivityLiveBinding;
+import com.fongmi.android.tv.event.RefreshEvent;
+import com.fongmi.android.tv.impl.Callback;
+import com.fongmi.android.tv.impl.ConfigCallback;
+import com.fongmi.android.tv.impl.CustomTarget;
+import com.fongmi.android.tv.impl.LiveCallback;
+import com.fongmi.android.tv.impl.PassCallback;
+import com.fongmi.android.tv.model.LiveViewModel;
+import com.fongmi.android.tv.player.PlayerHelper;
+import com.fongmi.android.tv.player.PlayerManager;
+import com.fongmi.android.tv.player.Source;
+import com.fongmi.android.tv.service.PlaybackService;
+import com.fongmi.android.tv.ui.adapter.ChannelAdapter;
+import com.fongmi.android.tv.ui.adapter.EpgDataAdapter;
+import com.fongmi.android.tv.ui.adapter.GroupAdapter;
+import com.fongmi.android.tv.ui.base.PlaybackActivity;
+import com.fongmi.android.tv.ui.custom.CustomKeyDownLive;
+import com.fongmi.android.tv.ui.custom.CustomLiveListView;
+import com.fongmi.android.tv.ui.custom.CustomSeekView;
+import com.fongmi.android.tv.ui.dialog.HistoryDialog;
+import com.fongmi.android.tv.ui.dialog.LiveDialog;
+import com.fongmi.android.tv.ui.dialog.PassDialog;
+import com.fongmi.android.tv.ui.dialog.SubtitleDialog;
+import com.fongmi.android.tv.ui.dialog.TrackDialog;
+import com.fongmi.android.tv.utils.Clock;
+import com.fongmi.android.tv.utils.ImgUtil;
+import com.fongmi.android.tv.utils.Notify;
+import com.fongmi.android.tv.utils.ResUtil;
+import com.fongmi.android.tv.utils.Traffic;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import androidx.media3.common.PlaybackException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Arrays;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.graphics.Color;
+
+
+public class LiveActivity extends PlaybackActivity implements GroupAdapter.OnClickListener, ChannelAdapter.OnClickListener, EpgDataAdapter.OnClickListener, CustomKeyDownLive.Listener, CustomLiveLi[...]
